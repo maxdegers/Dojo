5dd87f97b3531a2567fac8b6427ce43d894ebb04
Max <maxdegers@gmail.com>
2023-06-08 14:08:27 +0400

    print key pres

---
+++ print_key_pres.c
@@ -0,0 +1,24 @@
+/* boucle while key not press ,Print = basse 10 / base 2 / ASCII*/
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char input;
+    while (1 == 1)
+    {
+        // scanf("%c", &input);
+        input = getch();
+
+        printf("The input value is:%c\n", input);
+        printf("The decimal value is:%d\n", input);
+
+        printf("The binaire value is: ", input);
+        for (int i = 7; i >= 0; --i)
+        {
+            putchar((input & (1 << i)) ? '1' : '0');
+        }
+        printf("\n\n");
+    }
+
+    return 0;
+}
